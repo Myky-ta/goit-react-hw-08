eef63bf7b599e63e2f2d512c3e2d06ca0cf66068
Myky-ta <nikita24tenith@gmail.com>
2026-02-14 19:26:24 +0200

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -8,14 +8,35 @@ export default function LoginForm() {
   const dispatch = useDispatch();
 
   const handleSubmit = (values, { resetForm }) => {
-    dispatch(login(values));
+    dispatch(login(values)); // values = { email, password }
     resetForm();
   };
 
   return (
     <Formik initialValues={{ email: '', password: '' }} onSubmit={handleSubmit}>
       <Form className={styles.form}>
-        <Field type="email" name="email" placeholder="Email" className={styles.input} />
+        <Field
+          type="email"
+          name="email"
+          placeholder="Email"
+          autoComplete="email"
+          className={styles.input}
+          required
+        />
+        <Field
+          type="password"
+          name="password"
+          placeholder="Password"
+          autoComplete="current-password"
+          className={styles.input}
+          required
+        />
+        <button type="submit" className={styles.button}>Login</button>
+      </Form>
+    </Formik>
+  );
+}
+
         <Field type="password" name="password" placeholder="Password" className={styles.input} />
         <button type="submit" className={styles.button}>Login</button>
       </Form>
